5b9b34fcf9829ed3f21a8306f3e76fdefa844e23
Kauã <kaua.hilton@gmail.com>
2023-07-29 23:57:37 -0300

    primeiro

---
+++ js/page.js
@@ -0,0 +1,11 @@
+const strButton = document.querySelector('.str-button');
+
+strButton.addEventListener('click', () => {
+  window.location.href = 'game.html'
+})
+
+const strButton2 = document.querySelector('.str-button2');
+
+strButton2.addEventListener('click', () => {
+  window.location.href = 'game copy.html'
+})
